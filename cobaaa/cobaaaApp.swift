7c94144aef61d064989a716f29f1df9d591ad39f
diazabiyaksa <diaz.ap92@gmail.com>
2023-06-21 09:14:10 +0700

    Initial Commit

---
+++ cobaaa/cobaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobaaaApp.swift
+//  cobaaa
+//
+//  Created by Diaz Abiyaksa on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cobaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
